7489b2d8669dd7444fe07af6e0c4b640ccf5e992
Andy L <oleskainbox@gmail.com>
2026-07-23 17:19:08 -0400

    Added option to create new post.

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.forms import ModelForm
+from django import forms
 
 # Create your views here.
 def home_view(request):
@@ -7,4 +10,28 @@ def home_view(request):
     # print(request.META)
     # print('Request method:', request.method)
     
-    return render(request, 'posts/home.html')
+    posts = Post.objects.all()
+    
+    return render(request, 'posts/home.html', {'posts': posts})
+
+class PostCreateForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        labels = {
+            'body': 'Caption', 
+        }
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption...', 'class': 'font1 text-4xl'}),
+        }
+
+def post_create_view(request):
+    form = PostCreateForm()
+    
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    return render(request, 'posts/post_create.html', {'form': form})
